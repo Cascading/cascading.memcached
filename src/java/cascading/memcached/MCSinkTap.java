c1a7f3272c61cbb1e6dc785202cb965180e321ce
Chris K Wensel <chris@wensel.net>
2012-05-05 14:25:08 -0700

    use default equals/hashcode

--- src/java/cascading/memcached/MCSinkTap.java
+++ src/java/cascading/memcached/MCSinkTap.java
@@ -12,7 +12,6 @@ import cascading.flow.FlowProcess;
 import cascading.tap.SinkMode;
 import cascading.tap.SinkTap;
 import cascading.tuple.TupleEntryCollector;
-import cascading.util.Util;
 
 /**
  * Class MCSinkTap is a {@link cascading.tap.Tap} class only support sinking data to a Memcached cluster (or cluster supporting
@@ -90,43 +89,5 @@ public class MCSinkTap<Config> extends SinkTap<Config, Object>
   public long getModifiedTime( Config conf ) throws IOException
     {
     return 0; // always stale
-    }
-
-  @Override
-  public boolean equals( Object object )
-    {
-    if( this == object )
-      return true;
-    if( object == null || getClass() != object.getClass() )
-      return false;
-    if( !super.equals( object ) )
-      return false;
-
-    MCSinkTap mcSinkTap = (MCSinkTap) object;
-
-    if( useBinaryProtocol != mcSinkTap.useBinaryProtocol )
-      return false;
-    if( hostnames != null ? !hostnames.equals( mcSinkTap.hostnames ) : mcSinkTap.hostnames != null )
-      return false;
-
-    return true;
-    }
-
-  @Override
-  public int hashCode()
-    {
-    int result = super.hashCode();
-    result = 31 * result + ( hostnames != null ? hostnames.hashCode() : 0 );
-    result = 31 * result + ( useBinaryProtocol ? 1 : 0 );
-    return result;
-    }
-
-  @Override
-  public String toString()
-    {
-    if( hostnames != null )
-      return getClass().getSimpleName() + "[\"" + getScheme() + "\"]" + "[\"" + Util.sanitizeUrl( hostnames ) + "\"]"; // sanitize
-    else
-      return getClass().getSimpleName() + "[\"" + getScheme() + "\"]" + "[not initialized]";
     }
   }
